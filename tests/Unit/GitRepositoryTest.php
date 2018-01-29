3e68d25feeda81b6ef1f3da2de6f0432ed791bcf
Einar Gangsø <mail@einargangso.no>
2018-01-29 19:38:01 +0100

    Some basic tests for GitRepository

---
+++ tests/Unit/GitRepositoryTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Logg\Tests\Integration;
+
+use Logg\GitRepository;
+use PHPUnit\Framework\TestCase;
+
+class GitRepositoryTest extends TestCase
+{
+    /**
+     * @var GitRepository
+     */
+    private $repository;
+    
+    public function setUp()
+    {
+        $this->repository = new GitRepository(__DIR__ . '/../../');
+    }
+    
+    public function testCurrentBranchName()
+    {
+        return $this->assertEquals('master', $this->repository->getCurrentBranchName());
+    }
+
+    public function testGetLastCommitAuthor()
+    {
+        return $this->assertNotEmpty($this->repository->getLastCommitAuthor());
+    }
+
+    public function testGetLastCommitMessage()
+    {
+        return $this->assertNotEmpty($this->repository->getLastCommitMessage());
+    }
+}
