fb97a96efe5f0bd76dc5b819f053c9be8e4a1413
Einar Gangsø <mail@einargangso.no>
2018-02-03 08:43:26 +0100

    Test for author in KeepAChangelog

--- tests/Unit/KeepAChangelogFormatterTest.php
+++ tests/Unit/KeepAChangelogFormatterTest.php
@@ -15,54 +15,60 @@ class KeepAChangelogFormatterTest extends TestCase
         $result = $formatter->format('1.0', [
             new Entry('my-entry', [
                 'title' => 'Keep a changelog please',
-                'type' => 'fixed'
+                'type' => 'fixed',
+                'author' => 'EG'
             ]),
             
             new Entry('my-entry2', [
                 'title' => 'Keep a changelog please',
-                'type' => 'added'
+                'type' => 'added',
+                'author' => 'EG'
             ]),
             
             new Entry('my-entry2', [
                 'title' => 'Keep a changelog please',
-                'type' => 'removed'
+                'type' => 'removed',
+                'author' => 'EG'
             ]),
             
             new Entry('my-2ntryu', [
                 'title' => 'Keep a changelog please',
-                'type' => 'security'
+                'type' => 'security',
+                'author' => 'EG'
             ]),
             
             new Entry('my-2ntryu', [
                 'title' => 'Keep a changelog please',
-                'type' => 'changed'
+                'type' => 'changed',
+                'author' => 'EG'
             ]),
             
             new Entry('my-2ntryu', [
                 'title' => 'Keep a changelog please',
-                'type' => 'deprecated'
+                'type' => 'deprecated',
+                'author' => 'EG'
             ])
         ], []);
         
         $lines = [];
         $lines[] = '## [1.0] - ' . date('Y-m-d');
         $lines[] = '### Added';
-        $lines[] = '- Keep a changelog please';
+        $lines[] = '- Keep a changelog please (EG)';
         $lines[] = '';
         $lines[] = '### Changed';
-        $lines[] = '- Keep a changelog please';
+        $lines[] = '- Keep a changelog please (EG)';
         $lines[] = '';
         $lines[] = '### Deprecated';
-        $lines[] = '- Keep a changelog please';
+        $lines[] = '- Keep a changelog please (EG)';
         $lines[] = '';
         $lines[] = '### Removed';
-        $lines[] = '- Keep a changelog please';
+        $lines[] = '- Keep a changelog please (EG)';
         $lines[] = '';
         $lines[] = '### Fixed';
-        $lines[] = '- Keep a changelog please';
+        $lines[] = '- Keep a changelog please (EG)';
         $lines[] = '';
         $lines[] = '### Security';
-        $lines[] = '- Keep a changelog please';
+        $lines[] = '- Keep a changelog please (EG)';
         $lines[] = '';
         
         $this->assertEquals(implode("\n", $lines), $result);
